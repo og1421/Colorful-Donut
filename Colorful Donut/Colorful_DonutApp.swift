d42fdb9513e81d976ecc0e94ca7e08392f8d6bfc
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:43:05 -0300

    Initial Commit

---
+++ Colorful Donut/Colorful_DonutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorful_DonutApp.swift
+//  Colorful Donut
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Colorful_DonutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
